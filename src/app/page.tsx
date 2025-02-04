5fd4076bbc8c4395fdb6a21ad0ca4dfd7df87b99
megi <roy@shifters-ai.com>
2025-02-04 13:03:51 +0200

    Add Jotai for state management and enhance drag-and-drop functionality with file deletion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 
 import { Button } from "@/components/ui/button";
-import DragAndDrop from "@/components/dragAndDrop";
+import DragAndDrop from "@/components/dragAndDrop/dragAndDrop";
 import RobotConfig from "@/components/robotConfig";
 import React from "react";
+import { filesAtom } from "@/atoms/filesAtom";
+import { useAtomValue } from "jotai";
 
 const Home: React.FC = () => {
+  const files = useAtomValue(filesAtom);
+
+  const handleButtonClick = () => {
+    console.log(files);
+  };
+
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-base-200 dark:bg-gray-900">
       <h1 className="text-6xl font-bold text-primary dark:text-white absolute top-14">
@@ -17,7 +25,7 @@ const Home: React.FC = () => {
         <Button
           variant="secondary"
           className="mt-4 px-4 py-2 text-xl mr-4"
-          onClick={() => console.log("Cancel")}>
+          onClick={handleButtonClick}>
           Send
         </Button>
       </div>
