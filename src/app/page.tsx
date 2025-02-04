3d402fd16e02078c0866ccf48e2961ea0c3f6cfb
megi <roy@shifters-ai.com>
2025-02-04 11:17:19 +0200

    Enhance UI components: update header size and improve drag-and-drop area styling; replace button with custom Button component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import React from "react";
 const Home: React.FC = () => {
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-base-200 dark:bg-gray-900">
-      <h1 className="text-4xl font-bold text-primary dark:text-white">
+      <h1 className="text-6xl font-bold text-primary dark:text-white absolute top-14">
         Policy Hub
       </h1>
       <DragAndDrop />
--- src/components/ui/dragAndDrop.tsx
+++ src/components/ui/dragAndDrop.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { Button } from "@/components/ui/button";
 
 const DragAndDrop: React.FC = () => {
   const [files, setFiles] = useState<File[]>([]);
@@ -15,23 +16,27 @@ const DragAndDrop: React.FC = () => {
   return (
     <div>
       <div
-        className="mt-4 p-6 border-2 border-dashed h-[200px] w-[400px] border-gray-400 rounded-lg cursor-pointer"
+        className="mt-4 p-6 border-2 border-dashed h-[300px] w-[600px] border-gray-400 rounded-lg cursor-pointer"
         onDrop={handleDrop}
         onDragOver={handleDragOver}>
-        <p className="text-gray-500 flex justify-center items-center h-full">
+        <p className="text-xl text-gray-500 flex justify-center items-center h-full">
           Drag & Drop files or folders here
         </p>
       </div>
 
       <ul className="mt-4 text-gray-300 text-left">
         {files.map((file, index) => (
-          <li key={index}>{file.name}</li>
+          <li key={index}>{file.name} </li>
         ))}
       </ul>
-
-      <button onClick={() => console.log(files)} className="mt-4">
-        send
-      </button>
+      <div className="flex justify-center">
+        <Button
+          variant="secondary"
+          className="mt-4 px-4 py-2 text-xl mr-4"
+          onClick={() => setFiles([])}>
+          Send
+        </Button>
+      </div>
     </div>
   );
 };
